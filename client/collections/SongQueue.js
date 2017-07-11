5e623aec9708f08db6bb551d621060807d1174a3
Hack Reactor Student <student@hackreactor.com>
2017-07-10 18:56:41 -0700

    Pass Song Queue specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,44 +1,34 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
 
+  
   model: SongModel,
-  count: 0,
-
-  // initialize: function(song) {
-  //   if (song) {
-  //     this.queue.push(song);
-  //   }
-  // },
 
   initialize: function(queue) {
-    if (Array.isArray(queue)) {
-      for (var i = 0; i < queue.length; ++i) {
-        this.set(queue[i].title, queue[i]);
-      }       
-    } else if (queue) {
-      console.log(queue);
-      this.set(queue.title, queue);
-    }
+   
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+   
+    this.on('ended', function() {
+      this.shift();
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    });
+  
+    this.on('dequeue', function() {
+      this.shift();
+    });
+    
   },
 
-  add: function(song) {
-    this.set(song.title, song);
-    console.log(song);
-    if (this.length === 1) {
-      this.playFirst();
-    } 
-  },
 
   playFirst: function() {
-    //this.model.play();
+    this.at(0).play();
   },
-
-  //model.on('ended', ended);
-
-  ended: function() {
-    console.log('this ran');
-    this.queue.shift();
-  }
 
 
 });
